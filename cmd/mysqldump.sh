4a9e3b16194c0e87555277819a056d55a9f7e179
chengnaixue <chengnaixue@me.com>
2017-04-09 23:00:57 +0800

    bak all databases

--- cmd/mysqldump.sh
+++ cmd/mysqldump.sh
@@ -1,3 +1,4 @@
 mysqldump -uroot -proot new_schema>bak.sql
 mysqldump -uroot -proot new_schema teacher>bak_teacher.sql
 mysqldump -hlocalhost -uroot -proot --databases new_schema>file.dump
+mysqldump -uroot -proot --all-databases > all-databases.sql
